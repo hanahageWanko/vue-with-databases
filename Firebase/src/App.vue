e7391cae0e52d9ccf58fb801b2e8fd22be891dca
hanahagewanko <237s10mimi@gmail.com>
2023-03-05 22:55:18 +0900

    feat: Connect Firebase

--- Firebase/src/App.vue
+++ Firebase/src/App.vue
@@ -1,47 +1,72 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+/***********
+ * imports
+ */
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+import { db } from '../lib/firebase'
+import { collection, getDocs } from 'firebase/firestore'
+
+/***********
+ * types
+ */
+type iTasks = {
+  id: string
+  task?: string
+  completed?: boolean
+}
+
+/***********
+ * variables
+ */
+const tasks: Ref<(iTasks | null)[]> = ref([])
+const task: Ref<string> = ref('')
+
+/***********
+ * functions
+ */
+const getTasks = async () => {
+  const querySnapshot = await getDocs(collection(db, 'tasks'))
+  if (tasks.value) {
+    tasks.value = querySnapshot.docs.map((doc) => ({
+      ...doc.data(),
+      id: doc.id
+    }))
+  }
+}
+
+const addTask = () => {
+  tasks.value.push({ id: '', task: task.value, completed: false })
+  task.value = ''
+  id++
+}
+
+const deleteTask = (id) => {
+  const index = tasks.value.findIndex((task) => task.id === id)
+  tasks.value.splice(index, 1)
+}
+
+/***********
+ * calls
+ */
+getTasks()
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <h1>Tasks</h1>
+  <ul>
+    <li v-for="t in tasks" :key="t.id" :style="t.completed ? 'text-decoration:line-through' : ''">
+      <span><input type="checkbox" v-model="t.completed" /></span>
+      <span>{{ t.task }}</span>
+      <button @click="deleteTask(t.id)">削除</button>
+    </li>
+  </ul>
+  <form @submit.prevent="addTask">
+    <div>
+      <input v-model="task" />
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div>
+      <button type="submit">タスクを登録</button>
+    </div>
+  </form>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
