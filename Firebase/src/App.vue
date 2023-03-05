56deec96bb3e6f02a34364752de96d424866db5e
hanahagewanko <237s10mimi@gmail.com>
2023-03-05 23:14:27 +0900

    feat: fix CRUD method by fireStore

--- Firebase/src/App.vue
+++ Firebase/src/App.vue
@@ -2,10 +2,10 @@
 /***********
  * imports
  */
-import { ref } from 'vue'
+import { ref, onUnmounted } from 'vue'
 import type { Ref } from 'vue'
 import { db } from '../lib/firebase'
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, onSnapshot, addDoc, doc, deleteDoc, setDoc } from 'firebase/firestore'
 
 /***********
  * types
@@ -17,7 +17,7 @@ type iTasks = {
 }
 
 /***********
- * variables
+ * variablesonSnapshot
  */
 const tasks: Ref<(iTasks | null)[]> = ref([])
 const task: Ref<string> = ref('')
@@ -25,40 +25,57 @@ const task: Ref<string> = ref('')
 /***********
  * functions
  */
+
 const getTasks = async () => {
-  const querySnapshot = await getDocs(collection(db, 'tasks'))
-  if (tasks.value) {
+  // リアルタイム連携がしたいため、onSnapshotを使用
+  // リアルタイムでなくてもよい場合は、getDocs
+  const unsubscribe = onSnapshot(collection(db, 'tasks'), (querySnapshot) => {
     tasks.value = querySnapshot.docs.map((doc) => ({
       ...doc.data(),
       id: doc.id
     }))
-  }
+  })
 }
 
-const addTask = () => {
-  tasks.value.push({ id: '', task: task.value, completed: false })
+const addTask = async () => {
+  await addDoc(collection(db, 'tasks'), {
+    task: task.value,
+    completed: false
+  })
   task.value = ''
-  id++
 }
 
-const deleteTask = (id) => {
-  const index = tasks.value.findIndex((task) => task.id === id)
-  tasks.value.splice(index, 1)
+const deleteTask = async (id: string) => {
+  await deleteDoc(doc(db, 'tasks', id))
+}
+
+const updateTask = async (task: iTasks) => {
+  await setDoc(doc(db, 'tasks', task.id), {
+    task: task.task,
+    completed: task.completed
+  })
 }
 
 /***********
- * calls
+ * Calls
  */
 getTasks()
+
+/***********
+ * LifeCycles
+ */
+onUnmounted(() => {
+  getTasks()
+})
 </script>
 
 <template>
   <h1>Tasks</h1>
   <ul>
-    <li v-for="t in tasks" :key="t.id" :style="t.completed ? 'text-decoration:line-through' : ''">
-      <span><input type="checkbox" v-model="t.completed" /></span>
-      <span>{{ t.task }}</span>
-      <button @click="deleteTask(t.id)">削除</button>
+    <li v-for="t in tasks" :key="t!.id" :style="t!.completed ? 'text-decoration:line-through' : ''">
+      <span><input type="checkbox" v-model="t!.completed" @change="updateTask(t!)" /></span>
+      <span>{{ t!.task }}</span>
+      <button @click="deleteTask(t!.id)">削除</button>
     </li>
   </ul>
   <form @submit.prevent="addTask">
