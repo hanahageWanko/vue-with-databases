917220adc697c44aa2cae82b15c30f58afaad489
hanahagewanko <237s10mimi@gmail.com>
2023-03-11 03:57:38 +0900

    feat: Express.jsとVue.jsのConnect部分の作成

--- Prisma/index.ts
+++ Prisma/index.ts
@@ -3,6 +3,8 @@
  */
 import express from 'express'
 import type { Request, Response } from 'express'
+import { PrismaClient, Prisma } from '@prisma/client'
+import cors from 'cors'
 
 /**********
  * variables
@@ -10,12 +12,45 @@ import type { Request, Response } from 'express'
 const app = express()
 const port = 3000
 
-/**********
- * methods
- */
+app.use(cors())
+const prisma = new PrismaClient()
 
-// Response送信
+/********************************
+ * routing
+ */
 app.get('/', (req: Request, res: Response) => res.send('Hello World!'))
+
+app.get('/tasks', async (req: Request, res: Response) => {
+  const tasks = await prisma.tasks.findMany()
+  return res.json(tasks)
+})
+
+app.get('/users', async (req: Request, res: Response) => {
+  const users = await prisma.user.findMany()
+  return res.json(users)
+})
+
+app.post('/users', async (req: Request, res: Response) => {
+  const { name, email } = req.body
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email
+      }
+    })
+    return res.json(user)
+  } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      if (e.code === 'P2002') {
+        console.log(
+          'There is a unique constraint violation, a new user cannot be created with this email'
+        )
+      }
+    }
+    return res.status(400).json(e)
+  }
+})
 
 // listen
 app.listen(port, () => console.log(`Example app listening on port${port}!`))
--- Prisma/src/App.vue
+++ Prisma/src/App.vue
@@ -1,47 +1,74 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+/*********
+ * imports
+ */
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+
+/*********
+ * types
+ */
+
+// type iTasks = { [x: string]: string | number | boolean }[] | null
+type iTasks = {
+  id: number
+  task: string
+  completed: boolean
+}
+
+/**********
+ * variables
+ */
+const tasks: Ref<iTasks[]> = ref([])
+const task: Ref<string> = ref('')
+
+/**********
+ * functions
+ */
+const getTasks = async (): Promise<void> => {
+  const response = await fetch('http://localhost:3000/tasks')
+  const data = await response.json()
+  tasks.value = data
+}
+
+const addTask = (): void => {
+  tasks.value.push({ id: 0, task: task.value, completed: false })
+  task.value = ''
+  // id++
+}
+
+const deleteTask = (id: number): void => {
+  const index = tasks.value.findIndex((task) => task.id === id)
+  tasks.value.splice(index, 1)
+}
+
+/***********
+ * calls
+ */
+getTasks()
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <h1>Tasks</h1>
+  <ul>
+    <li
+      v-for="task in tasks"
+      :key="task.id"
+      :style="task.completed ? 'text-decoration:line-through' : ''"
+    >
+      <span><input type="checkbox" v-model="task.completed" /></span>
+      <span>{{ task.task }}</span>
+      <button @click="deleteTask(task.id)">削除</button>
+    </li>
+  </ul>
+  <form @submit.prevent="addTask">
+    <div>
+      <input v-model="task" />
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div>
+      <button type="submit">タスクを登録</button>
+    </div>
+  </form>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
